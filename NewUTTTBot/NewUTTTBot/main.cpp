48df5c54c4a77847361526603e6a802a8f9876b8
taylorules <taylor.p.santos@gmail.com>
2016-09-14 17:34:20 -0700

    Performance improvements

--- NewUTTTBot/NewUTTTBot/main.cpp
+++ NewUTTTBot/NewUTTTBot/main.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <assert.h>
 #include <sstream>
+#include <vector>
 
 #define INT_MAX 2147483647
 #define INT_MIN -2147483647
@@ -14,13 +15,452 @@ using namespace std;
 
 const int magicSquare[3][3] = { {8,1,6}, {3,5,7}, {4,9,2} };
 const int scoreMultiplyer[3][3] = { { 3,2,3 },{ 2,4,2 },{ 3,2,3 } };
+const char boxCoord[9]  = { 0,1,2,0,1,2,0,1,2 };
+const char gridCoord[9] = { 0,0,0,1,1,1,2,2,2 };
+const char adjacentCoord[3] =  { 1,2,0 };
+const char adjacentCoord2[3] = { 2,0,1 };
 const int scorePerGrid = 20;
 
 int getOpponent(int player)
 {
 	/*Takes an input player-id and returns the opposing player-id*/
-	assert(player == 1 || player == 2);
 	return !(player - 1) + 1;
+}
+
+class board_simple
+{
+public:
+	char field[3][3][3][3];
+	signed char macroBoard[3][3];
+	short int gridScores[3][3] = { {0,0,0},{ 0,0,0 },{ 0,0,0 } };
+	short int score = 0;
+	char winner = 0;
+
+	void play_move(char player, char fieldX, char fieldY);
+	short int get_score(char player);
+
+	board_simple* copy();
+
+	board_simple();
+	~board_simple();
+};
+
+board_simple::board_simple()
+{
+
+}
+
+board_simple::~board_simple()
+{
+
+}
+
+board_simple* board_simple::copy()
+{
+	board_simple* newBoard = new board_simple();
+	memcpy(newBoard, this, sizeof(board_simple));
+	return newBoard;
+}
+
+void board_simple::play_move(char player, char fieldX, char fieldY)
+{
+	//int x = fieldX % 3;
+	//int y = fieldY % 3;
+	//int gridX = fieldX / 3;
+	//int gridY = fieldY / 3;
+	char x = boxCoord[fieldX];
+	char y = boxCoord[fieldY];
+	char gridX = gridCoord[fieldX];
+	char gridY = gridCoord[fieldY];
+
+	score -= gridScores[gridX][gridY];
+	field[gridX][gridY][x][y] = player;
+	/*
+	char horiz = 1 << (player-1);
+	horiz &= 1 << (field[gridX][gridY][(x + 1) % 3][y]-1);
+	horiz &= 1 << (field[gridX][gridY][(x + 2) % 3][y]-1);
+	char vert = 1 << (player - 1);
+	vert &= 1 << (field[gridX][gridY][x][(y + 1) % 3] - 1);
+	vert &= 1 << (field[gridX][gridY][x][(y + 2) % 3] - 1);
+
+	char isDiag = !(x - y);
+	char diag = isDiag << (player - 1);
+	diag &= isDiag << (field[gridX][gridY][(x + 1) % 3][(y + 1) % 3] - 1);
+	diag &= isDiag << (field[gridX][gridY][(x + 2) % 3][(y + 2) % 3] - 1);
+
+	char isADiag = !(x - (2 - y));
+	char aDiag = isADiag << (player - 1);
+	aDiag &= isADiag << (field[gridX][gridY][(x + 2) % 3][(y + 1) % 3] - 1);
+	aDiag &= isADiag << (field[gridX][gridY][(x + 1) % 3][(y + 2) % 3] - 1);
+
+	macroBoard[gridX][gridY] = horiz | vert | diag | aDiag;
+	*/
+	
+	if (field[gridX][gridY][adjacentCoord[x]][y] == player && field[gridX][gridY][adjacentCoord2[x]][y] == player)
+	{
+		macroBoard[gridX][gridY] = player;
+	}
+	else if (field[gridX][gridY][x][adjacentCoord[y]] == player && field[gridX][gridY][x][adjacentCoord2[y]] == player)
+	{
+		macroBoard[gridX][gridY] = player;
+	}
+	else if (x == y && field[gridX][gridY][adjacentCoord[x]][adjacentCoord[y]] == player && field[gridX][gridY][adjacentCoord2[x]][adjacentCoord2[y]] == player)
+	{
+		macroBoard[gridX][gridY] = player;
+	}
+	else if (x == 2 - y && field[gridX][gridY][adjacentCoord2[x]][adjacentCoord[y]] == player && field[gridX][gridY][adjacentCoord[x]][adjacentCoord2[y]] == player)
+	{
+		macroBoard[gridX][gridY] = player;
+	}
+	
+
+	if (macroBoard[gridX][gridY] == player)
+	{
+		//gridScores[gridX][gridY] = (player == 1 ? 1 : -1) * scorePerGrid * scoreMultiplyer[gridX][gridY];
+		gridScores[gridX][gridY] = 0;
+		int opponent = getOpponent(player);
+		if (macroBoard[adjacentCoord[gridX]][gridY] != opponent && macroBoard[adjacentCoord[gridX]][gridY] != 3 &&
+			macroBoard[adjacentCoord2[gridX]][gridY] != opponent && macroBoard[adjacentCoord2[gridX]][gridY] != 3)
+		{
+			gridScores[gridX][gridY] += (player == 1 ? 1 : -1) * scorePerGrid;
+		}
+		if (macroBoard[gridX][adjacentCoord[gridY]] != opponent && macroBoard[gridX][adjacentCoord[gridY]] != 3 &&
+			macroBoard[gridX][adjacentCoord2[gridY]] != opponent && macroBoard[gridX][adjacentCoord2[gridY]] != 3)
+		{
+			gridScores[gridX][gridY] += (player == 1 ? 1 : -1) * scorePerGrid;
+		}
+		if (gridX == gridY && macroBoard[adjacentCoord[gridX]][adjacentCoord[gridY]] != opponent && macroBoard[adjacentCoord[gridX]][adjacentCoord[gridY]] != 3 &&
+			macroBoard[adjacentCoord2[gridX]][adjacentCoord2[gridY]] != opponent && macroBoard[adjacentCoord2[gridX]][adjacentCoord2[gridY]] != 3)
+		{
+			gridScores[gridX][gridY] += (player == 1 ? 1 : -1) * scorePerGrid;
+		}
+		if (gridX == 2 - gridY && macroBoard[adjacentCoord2[gridX]][adjacentCoord[gridY]] != opponent && macroBoard[adjacentCoord2[gridX]][adjacentCoord[gridY]] != 3 &&
+			macroBoard[adjacentCoord[gridX]][adjacentCoord2[gridY]] != opponent && macroBoard[adjacentCoord[gridX]][adjacentCoord2[gridY]] != 3)
+		{
+			gridScores[gridX][gridY] += (player == 1 ? 1 : -1) * scorePerGrid;
+		}
+
+		if (macroBoard[adjacentCoord[gridX]][gridY] == player && macroBoard[adjacentCoord2[gridX]][gridY] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (macroBoard[gridX][adjacentCoord[gridY]] == player && macroBoard[gridX][adjacentCoord2[gridY]] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (gridX == gridY && macroBoard[adjacentCoord[gridX]][adjacentCoord[gridY]] == player && macroBoard[adjacentCoord2[gridX]][adjacentCoord2[gridY]] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (fieldX == 2 - fieldY && macroBoard[adjacentCoord2[gridX]][adjacentCoord[gridY]] == player && macroBoard[adjacentCoord[gridX]][adjacentCoord2[gridY]] == player)
+		{
+			winner = player;
+			return;
+		}
+	}
+	else {
+		gridScores[gridX][gridY] = 0;
+		for (int boxY = 0; boxY < 3; ++boxY)
+		{
+			for (int boxX = 0; boxX < 3; ++boxX)
+			{
+				if (field[gridX][gridY][boxX][boxY] == 1)
+				{
+					gridScores[gridX][gridY] += scoreMultiplyer[boxX][boxY];
+				}
+				else if (field[gridX][gridY][boxX][boxY] == 2)
+				{
+					gridScores[gridX][gridY] -= scoreMultiplyer[boxX][boxY];
+				}
+			}
+		}
+	}
+
+//	nextMovesX->clear();
+//	nextMovesY->clear();
+
+	bool openBoard = false;
+	if (macroBoard[x][y] > 0)
+	{
+		openBoard = true;
+	}
+	for (int Y = 0; Y < 3; ++Y)
+	{
+		for (int X = 0; X < 3; ++X)
+		{
+			if (macroBoard[X][Y] <= 0)
+			{
+				if (openBoard || (X == x&&Y == y))
+				{
+					macroBoard[X][Y] = -1;
+/*					for (int boxY = 0; boxY < 3; ++boxY)
+					{
+						for (int boxX = 0; boxX < 3; ++boxX)
+						{
+							if (field[X][Y][boxX][boxY] == 0)
+							{
+								nextMovesX->push_back(3 * X + boxX);
+								nextMovesY->push_back(3 * Y + boxY);
+							}
+						}
+					}
+*/				}
+				else
+					macroBoard[X][Y] = 0;
+			}
+		}
+	}
+
+	score += gridScores[gridX][gridY];
+
+	/*
+	if (macroBoard[gridX][gridY] <= 0)
+	{
+		//gridScores[gridX][gridY] = -(player * 2 - 3) * scoreMultiplyer[x][y];
+		
+		gridScores[gridX][gridY] = 0;		
+		for (int i = 0; i < 3; ++i)
+		{
+			int horiz1Count = 0;
+			int horiz2Count = 0;
+			int vert1Count = 0;
+			int vert2Count = 0;
+			for (int j = 0; j < 3; ++j)
+			{
+				if (field[gridX][gridY][i][j] == 1)
+					horiz1Count++;
+				else if (field[gridX][gridY][i][j] == 2)
+					horiz2Count++;
+
+				if (field[gridX][gridY][j][i] == 1)
+					vert1Count++;
+				else if (field[gridX][gridY][j][i] == 2)
+					vert1Count++;
+			}
+			if (horiz1Count == 0)
+			{
+				gridScores[gridX][gridY] -= horiz2Count*horiz2Count;
+			}
+			else if (horiz2Count == 0)
+			{
+				gridScores[gridX][gridY] += horiz1Count*horiz1Count;
+			}
+			if (vert1Count == 0)
+			{
+				gridScores[gridX][gridY] -= vert2Count*vert2Count;
+			}
+			else if (vert2Count == 0)
+			{
+				gridScores[gridX][gridY] += vert1Count*vert1Count;
+			}
+		}
+		int diag1Count = 0;
+		int diag2Count = 0;
+		int aDiag1Count = 0;
+		int aDiag2Count = 0;
+		if (field[gridX][gridY][0][0] == 1)
+			diag1Count++;
+		else if (field[gridX][gridY][0][0] == 2)
+			diag2Count++;
+		if (field[gridX][gridY][2][2] == 1)
+			diag1Count++;
+		else if (field[gridX][gridY][2][2] == 2)
+			diag2Count++;
+
+		if (field[gridX][gridY][2][0] == 1)
+			aDiag1Count++;
+		else if (field[gridX][gridY][2][0] == 2)
+			aDiag2Count++;
+		if (field[gridX][gridY][0][2] == 1)
+			aDiag1Count++;
+		else if (field[gridX][gridY][0][2] == 2)
+			aDiag2Count++;
+
+		if (field[gridX][gridY][1][1] == 1)
+		{
+			diag1Count++;
+			aDiag1Count++;
+		}
+		else if (field[gridX][gridY][1][1] == 2)
+		{
+			diag2Count++;
+			aDiag2Count++;
+		}
+
+		if (diag1Count == 0)
+		{
+			gridScores[gridX][gridY] -= diag2Count*diag2Count;
+		}
+		else if (diag2Count == 0)
+		{
+			gridScores[gridX][gridY] += diag1Count*diag2Count;
+		}
+		if (aDiag1Count == 0)
+		{
+			gridScores[gridX][gridY] -= aDiag2Count*aDiag2Count;
+		}
+		else if (aDiag2Count == 0)
+		{
+			gridScores[gridX][gridY] += aDiag1Count*aDiag1Count;
+		}
+
+		macroBoard[gridX][gridY] = 3;
+		for (int boxY = 0; boxY < 3; ++boxY)
+		{
+			for (int boxX = 0; boxX < 3; ++boxX)
+			{
+				if (field[gridX][gridY][boxX][boxY] == 0)
+				{
+					macroBoard[gridX][gridY] = 0;
+					goto noTie;
+				}
+			}
+		}
+	}
+	else if (macroBoard[gridX][gridY] == player)
+	{
+		if (macroBoard[(gridX + 1) % 3][gridY] == player && macroBoard[(gridX + 2) % 3][gridY] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (macroBoard[gridX][(gridY + 1) % 3] == player && macroBoard[gridX][(gridY + 2) % 3] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (gridX == gridY && macroBoard[(gridX + 1) % 3][(gridY + 1) % 3] == player && macroBoard[(gridX + 2) % 3][(gridY + 2) % 3] == player)
+		{
+			winner = player;
+			return;
+		}
+		if (fieldX == 2 - fieldY && macroBoard[(gridX + 2) % 3][(gridY + 1) % 3] == player && macroBoard[(gridX + 1) % 3][(gridY + 2) % 3] == player)
+		{
+			winner = player;
+			return;
+		}
+
+		//gridScores[gridX][gridY] = (player == 1 ? 1 : -1) * scorePerGrid * scoreMultiplyer[gridX][gridY];
+
+		
+		int thisGridScore = 0;
+		int opponent = getOpponent(player);
+		if (macroBoard[(gridX + 1) % 3][gridY] != opponent && macroBoard[(gridX + 2) % 3][gridY] != opponent &&
+			macroBoard[(gridX + 1) % 3][gridY] != 3 && macroBoard[(gridX + 2) % 3][gridY] != 3)
+		{
+			int ownedCount = 1;
+			if (macroBoard[(gridX + 1) % 3][gridY] == player)
+				ownedCount++;
+			if (macroBoard[(gridX + 2) % 3][gridY] == player)
+				ownedCount++;
+			if (ownedCount == 3)
+			{
+				winner = player;
+				return;
+			}
+			thisGridScore += pow(scorePerGrid, ownedCount);
+		}
+		if (macroBoard[gridX][(gridY + 1) % 3] != opponent && macroBoard[gridX][(gridY + 2) % 3] != opponent &&
+			macroBoard[gridX][(gridY + 1) % 3] != 3 && macroBoard[gridX][(gridY + 2) % 3] != 3)
+		{
+			int ownedCount = 1;
+			if (macroBoard[gridX][(gridY + 1) % 3] == player)
+				ownedCount++;
+			if (macroBoard[gridX][(gridY + 2) % 3] == player)
+				ownedCount++;
+			if (ownedCount == 3)
+			{
+				winner = player;
+				return;
+			}
+			thisGridScore += pow(scorePerGrid, ownedCount);
+		}
+		if (fieldX == fieldY)
+		{
+			if (macroBoard[(gridX + 1) % 3][(gridY + 1) % 3] != opponent && macroBoard[(gridX + 2) % 3][(gridY + 2) % 3] != opponent &&
+				macroBoard[(gridX + 1) % 3][(gridY + 1) % 3] != 3 && macroBoard[(gridX + 2) % 3][(gridY + 2) % 3] != 3)
+			{
+				int ownedCount = 1;
+				if (macroBoard[(gridX + 1) % 3][(gridY + 1) % 3] == player)
+					ownedCount++;
+				if (macroBoard[(gridX + 2) % 3][(gridY + 2) % 3] == player)
+					ownedCount++;
+				if (ownedCount == 3)
+				{
+					winner = player;
+					return;
+				}
+				thisGridScore += pow(scorePerGrid, ownedCount);
+			}
+		}
+		if (fieldX == 2 - fieldY)
+		{
+			if (macroBoard[(gridX + 2) % 3][(gridY + 1) % 3] != opponent && macroBoard[(gridX + 1) % 3][(gridY + 2) % 3] != opponent &&
+				macroBoard[(gridX + 2) % 3][(gridY + 1) % 3] != 3 && macroBoard[(gridX + 1) % 3][(gridY + 2) % 3] != 3)
+			{
+				int ownedCount = 1;
+				if (macroBoard[(gridX + 2) % 3][(gridY + 1) % 3] == player)
+					ownedCount++;
+				if (macroBoard[(gridX + 1) % 3][(gridY + 2) % 3] == player)
+					ownedCount++;
+				if (ownedCount == 3)
+				{
+					winner = player;
+					return;
+				}
+				thisGridScore += pow(scorePerGrid, ownedCount);
+			}
+		}
+		gridScores[gridX][gridY] = player == 1 ? thisGridScore : -thisGridScore;
+
+	}
+noTie:
+	bool openBoard = false;
+	if (macroBoard[x][y] > 0)
+	{
+		openBoard = true;
+	}
+	for (int Y = 0; Y < 3; ++Y)
+	{
+		for (int X = 0; X < 3; ++X)
+		{
+			if (macroBoard[X][Y] <= 0)
+			{
+				if (openBoard || (X == x&&Y == y))
+					macroBoard[X][Y] = -1;
+				else
+					macroBoard[X][Y] = 0;
+			}
+		}
+	}
+	*/
+}
+
+short int board_simple::get_score(char player)
+{
+	if (winner == 0)
+	{
+		/*
+		short int score = 0;
+		for (int gridY = 0; gridY < 3; ++gridY)
+		{
+			for (int gridX = 0; gridX < 3; ++gridX)
+			{
+				score += gridScores[gridX][gridY];
+			}
+		}
+		*/
+		return (player == 1 ? score : -score);
+	}
+	else if (winner == 1)
+		return player == 1 ? SHRT_MAX : SHRT_MIN;
+	else if (winner == 2)
+		return player == 2 ? SHRT_MAX : SHRT_MIN;
+	else
+		return 0;
 }
 
 class board {
@@ -828,25 +1268,6 @@ int alphaBetaWithCount(board* b, int player, bool maximizing, int alpha, int bet
 	return bestScore;
 }
 
-/*
-int MTDF(board* b, int f, int player, bool maximizing, int depth)
-{
-	int g = f;
-	int upperBound = INT_MAX;
-	int lowerBound = INT_MIN;
-	while (lowerBound < upperBound)
-	{
-		int beta = max(g, lowerBound + 1);
-		g = alphaBetaWithDepth(b, depth, player, maximizing, beta - 1, beta);
-		if (g < beta)
-			upperBound = g;
-		else
-			lowerBound = g;
-	}
-	return g;
-}
-*/
-
 int MTDF_With_Count(board* b, int f, int player, bool maximizing, int* count)
 {
 	int g = f;
@@ -864,8 +1285,109 @@ int MTDF_With_Count(board* b, int f, int player, bool maximizing, int* count)
 	return g;
 }
 
+short int miniMax_simple(board_simple* b, char player, short int alpha, short int beta, char depth, unsigned int* count)
+{
+	if (depth == 0 || b->winner != 0)
+	{
+		(*count)++;
+		return b->get_score(player);
+	}
+	int bestScore = SHRT_MIN+1;
+	for (int gridY = 0; gridY < 3; ++gridY)
+	{
+		for (int gridX = 0; gridX < 3; ++gridX)
+		{
+			if (b->macroBoard[gridX][gridY] == -1)
+			{
+				for (int y = 0; y < 3; ++y)
+				{
+					for (int x = 0; x < 3; ++x)
+					{
+						if (b->field[gridX][gridY][x][y] == 0)
+						{
+							board_simple* newBoard = b->copy();
+							newBoard->play_move(player, 3 * gridX + x, 3 * gridY + y);
+							int score = -miniMax_simple(newBoard, getOpponent(player), -beta, -alpha, depth - 1, count);
+							delete newBoard;
+							if (score == SHRT_MAX)
+								return score;
+							if (score > bestScore)
+							{
+								bestScore = score;
+							}
+
+							if (score > alpha)
+							{
+								alpha = score;
+							}
+							if (beta <= alpha)
+								return beta;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return bestScore;
+}
+
+short int MDTF_simple(board_simple* b, char player, char depth, unsigned int* count, short int f)
+{
+	short int g = f;
+	short int upperBound = SHRT_MAX;
+	short int lowerBound = SHRT_MIN+1;
+	while (lowerBound < upperBound)
+	{
+		short int beta = g;
+		if (lowerBound + 1 > g)
+			beta = lowerBound + 1;
+		g = miniMax_simple(b, player, beta - 1, beta, depth, count);
+		if (g < beta)
+			upperBound = g;
+		else
+			lowerBound = g;
+	}
+	return g;
+}
+
 int main()
 {
+	while (1) //Simple
+	{
+		board_simple* b = new board_simple();
+//		b->nextMovesX = new vector<int>();
+//		b->nextMovesY = new vector<int>();
+		for (int gridY = 0; gridY < 3; ++gridY)
+		{
+			for (int gridX = 0; gridX < 3; ++gridX)
+			{
+				for (int y = 0; y < 3; ++y)
+				{
+					for (int x = 0; x < 3; ++x)
+					{
+						b->field[gridX][gridY][x][y] = 0;
+//						b->nextMovesX->push_back(3 * gridX + x);
+//						b->nextMovesY->push_back(3 * gridY + y);
+					}
+				}
+				b->macroBoard[gridX][gridY] = -1;
+			}
+		}
+		
+		char depth = 11;
+		unsigned int count = 0;
+		int start = clock();
+		//short int score = MDTF_simple(b, 1, depth, &count, 4);
+		short int score = miniMax_simple(b, 1, SHRT_MIN+1, SHRT_MAX, depth, &count);
+		int stop = clock();
+		float actualTime = ((stop - start) / (CLOCKS_PER_SEC / 1000.0f));
+		float nodesPerMs = count / actualTime;
+		cout << "Depth " << (int)depth << ": Score " << score << " with " << count << " nodes found in " << actualTime << " ms. (" << (int)(nodesPerMs * 1000) << " nodes per second)" << endl;
+		while (1);
+	}
+
+
 	int max_timebank = 0;
 	int time_per_move = 0;
 	char player_names[16];
